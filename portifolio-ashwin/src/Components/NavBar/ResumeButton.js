c7114fe6946e1c7bddf3e11ff45c22f0747d6afc
Ashwin Balaji Dhandapani <60782187+ashwn7@users.noreply.github.com>
2022-06-08 01:18:19 -0400

    Update ResumeButton.js

--- portifolio-ashwin/src/Components/NavBar/ResumeButton.js
+++ portifolio-ashwin/src/Components/NavBar/ResumeButton.js
@@ -5,7 +5,7 @@ function ResumeButton() {
   return (
     <Link
       className="navItem resumeButton"
-      to="/Resume_AshwinBalajiDhandapani.pdf"
+      to="/public/Resume_Dhandapani_AshwinBalaji_SDE.pdf"
       target="_blank"
     >
       <h3>Download Resume</h3>
